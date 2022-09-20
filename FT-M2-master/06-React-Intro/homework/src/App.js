b25e3126090c80b249ab73631784720d8ec4289e
jonasojeda <jonojed98@gmail.com>
2022-09-20 18:38:29 -0300

    Inicio de diseño en React

--- FT-M2-master/06-React-Intro/homework/src/App.js
+++ FT-M2-master/06-React-Intro/homework/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        Weather
       </div>
       <hr />
       <div>
--- FT-M2-master/06-React-Intro/homework/src/components/Card.jsx
+++ FT-M2-master/06-React-Intro/homework/src/components/Card.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
+import s from '../styles/Card.module.css';
 export default function Card(props) {
   // acá va tu código
   
-  return <div>
-    <button onClick={props.onClose}>x{props.onClose}</button>
+  return <>
+  <div className={s.card}>
+    <button onClick={props.onClose} className={s.btn} >x</button>
     <h1>Max {props.max}</h1>
     <h1>Min {props.min}</h1>
     <h1>{props.name}</h1>`
     <img src={`http://openweathermap.org/img/wn/${props.img}@2x.png`} alt="" />
   </div>
+
+          </>
 };
