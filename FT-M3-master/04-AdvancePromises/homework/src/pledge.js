65bad3ddecc1feaaf754851bcf84a397d4d4a011
jonasojeda <jonojed98@gmail.com>
2022-10-06 19:46:07 -0300

    04-AdvancePromises (Capitulo I COMPLETADO)

--- FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -3,7 +3,39 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+function $Promise(executor){
 
+    this._state = "pending"
+
+    if(typeof executor !== "function"){    
+        throw new TypeError("The order is executor, function")
+        
+    }
+        //Bindead
+    executor(
+        this._internalResolve.bind(this), 
+        this._internalReject.bind(this) ) //Se puede bindear
+
+        //arrow function
+    //executor(
+        //(value)=>{this._internalResolve(value)}, 
+        //(value)=>{this._internalReject(this)}); // o Usar una arrow function
+}
+
+$Promise.prototype._internalResolve = function (value){
+    if(this._state =="pending" ){
+        this._state = 'fulfilled';
+        this._value = value;
+    }
+};
+
+$Promise.prototype._internalReject = function(value){
+    if(this._state =="pending" ){
+        this._state = 'rejected';
+        this._value = value;
+    }
+    
+};
 
 
 module.exports = $Promise;
