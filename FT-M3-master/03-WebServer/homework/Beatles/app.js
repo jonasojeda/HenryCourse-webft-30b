f484457e94a873bac7c6bc2978419ac589a571d4
jonasojeda <jonojed98@gmail.com>
2022-10-06 13:24:12 -0300

    03-WebServer (Completo)

--- FT-M3-master/03-WebServer/homework/Beatles/app.js
+++ FT-M3-master/03-WebServer/homework/Beatles/app.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var fs   = require('fs');
-
 var beatles=[{
   name: "John Lennon",
   birthdate: "09/10/1940",
@@ -22,3 +21,56 @@ var beatles=[{
   profilePic:"http://cp91279.biography.com/BIO_Bio-Shorts_0_Ringo-Starr_SF_HD_768x432-16x9.jpg"
 }
 ]
+
+http.createServer((req,res)=>{
+  // res.writeHead(200,{ 'Content-Type':'text/plain' })
+  // res.end('Ejercicio Beattles')
+  if(req.url === '/api'){
+    res.writeHead(200,{'content-type': 'application/json'});
+    res.end(JSON.stringify(beatles));
+  }
+
+  // /api/John Lennon
+  // /api/Paul McCartney
+  // /api/George Harrison
+  // /api/Richard Starkey
+  if(req.url.substring(0,5)=== '/api/' && req.url.length>5 ){
+    //url = /api/ALGO
+    //req.url.substring(0,5) = /api/
+    //req.url.length = 9
+    let findBeatle = req.url.split('/').pop(); //ALGO
+    let foundBeatle = beatles.find((beatle)=> findBeatle === encodeURI(beatle.name))
+
+    if(foundBeatle){
+      res.writeHead(200,{'content-type': 'application/json'});
+      res.end(JSON.stringify(foundBeatle));
+    }else{
+      res.writeHead(404,{'content-type':'text/plain'});
+      res.end('No exites ese Beatle')
+    }
+  }
+  if(req.url === '/'){
+    res.writeHead(200, {'content-type':'text/html'});
+
+    const index =  fs.readFileSync(`${__dirname}/index.html`); //'/index.html/'
+    res.end(index)
+  }
+
+  // /Jhon Lennos
+  let findBeatle = req.url.split('/').pop(); //ALGO
+  let foundBeatle = beatles.find((b)=> findBeatle === encodeURI(b.name));
+  console.log(foundBeatle)
+  if(foundBeatle){
+    let read =  fs.readFileSync(`${__dirname}/beatle.html`,'utf-8')
+    read = read.replace(/{name}/g, foundBeatle.name)
+    read = read.replace(/{birth}/, foundBeatle.birthdate)
+    read = read.replace(/{name}/, foundBeatle.name)
+    read = read.replace(/{profilePic}/, foundBeatle.profilePic)
+    res.end(read)
+  }else{
+    res.writeHead(404, {'content-type':'text/plain'});
+    res.end('No exite ese Beatle')
+  }
+
+
+}).listen(3000, '127.0.0.1');
--- FT-M3-master/03-WebServer/homework/showByName/app.js
+++ FT-M3-master/03-WebServer/homework/showByName/app.js
@@ -2,3 +2,15 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer( function(req, res){ 
+    fs.readFile(`${__dirname}/images/${req.url}.jpg`,(err,data)=>{
+        if(err){
+            res.writeHead(404, {"content-type":"text/plain"});
+            res.end("Perro inexistente")
+        }else{
+            res.writeHead(200, {"content-type":"image/jpg"});
+            res.end(data);
+        }
+    });
+	
+}).listen(1337, '127.0.0.1');
