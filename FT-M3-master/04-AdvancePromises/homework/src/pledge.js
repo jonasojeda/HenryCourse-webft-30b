5618bca0af15fc317eb776e97d92bc7092d87603
jonasojeda <jonojed98@gmail.com>
2022-10-07 13:32:08 -0300

    04-AdvancePromises (Capitulo II COMPLETADO)

--- FT-M3-master/04-AdvancePromises/homework/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/src/pledge.js
@@ -6,6 +6,8 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 function $Promise(executor){
 
     this._state = "pending"
+    this._value= undefined
+    this._handlerGroups=[]
 
     if(typeof executor !== "function"){    
         throw new TypeError("The order is executor, function")
@@ -16,7 +18,7 @@ function $Promise(executor){
         this._internalResolve.bind(this), 
         this._internalReject.bind(this) ) //Se puede bindear
 
-        //arrow function
+    //arrow function
     //executor(
         //(value)=>{this._internalResolve(value)}, 
         //(value)=>{this._internalReject(this)}); // o Usar una arrow function
@@ -26,6 +28,8 @@ $Promise.prototype._internalResolve = function (value){
     if(this._state =="pending" ){
         this._state = 'fulfilled';
         this._value = value;
+
+        this._callHandlers();
     }
 };
 
@@ -33,10 +37,37 @@ $Promise.prototype._internalReject = function(value){
     if(this._state =="pending" ){
         this._state = 'rejected';
         this._value = value;
+
+        this._callHandlers();
     }
     
 };
 
+$Promise.prototype.then = function(successCb, errorCb){
+    this._handlerGroups.push({
+        successCb: typeof successCb === 'function' ? successCb :false, //ternarios 
+        errorCb: typeof errorCb === 'function' ? errorCb :false, // ternario
+    })
+
+    this._callHandlers();
+
+};
+$Promise.prototype._callHandlers=function(){
+    if(this._state !== 'pending'){
+        while(this._handlerGroups.length){
+            const{successCb, errorCb}= this._handlerGroups.shift();
+            if(this._state === 'fulfilled'){
+                if(successCb){
+                    successCb(this._value);
+                }
+            }
+        }
+    }else if(this._state === 'rejected'){
+        if(errorCb){
+            errorCb(this._value);
+        }
+    }
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
